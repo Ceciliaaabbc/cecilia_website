c8dc11d3a1f7700bf10f7f5f18a2d5f8e626ab06
cecilia <142327962+Ceciliasta97@users.noreply.github.com>
2026-06-07 13:30:37 +0100

    project html

--- src/main/java/com/website/HomeController.java
+++ src/main/java/com/website/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "projects";
+    }
 }
